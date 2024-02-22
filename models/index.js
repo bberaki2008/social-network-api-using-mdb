463b0f343254de0b5d9f49340b8bf9a6398c5888
Berie <bberaki2015@gmail.com>
2024-02-21 21:40:19 -0800

    created thoughtController.js, userController.js, anduser, thought models as well as reaction schema.

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
-const Post = require('./Post');
-const Comment = require('./Comment');
+const Thought = require('./Thought');
+const User = require('./User');
+const Reaction = require('./Reaction');
 
-module.exports = { Post, Comment };
+module.exports = { Thought, User, Reaction };
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought, //(don't forget to push the created thought's _id to the associated user's thoughts array field)
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
